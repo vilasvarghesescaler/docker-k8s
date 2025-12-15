60659c4bd12d8f23d7444729954c0031f7a0ffde
vilasvarghesescaler <vilas.varghese_1@scaler.com>
2025-12-15 19:42:13 +0530

    Comment out ECR login and apply commands in script
    Comment out AWS ECR login and Kubernetes apply commands.

--- scripts/apply_manifests.sh
+++ scripts/apply_manifests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 396982710430.dkr.ecr.us-east-1.amazonaws.com
+#aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 396982710430.dkr.ecr.us-east-1.amazonaws.com
 kubectl run test --image=nginx
-kubectl apply -f /opt/k8s/deploy-tomcat.yaml
+#kubectl apply -f /opt/k8s/deploy-tomcat.yaml
